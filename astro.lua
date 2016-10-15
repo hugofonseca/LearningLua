8e54cc45975fde4df9d9ed05ac6383498caf142c
hugofonseca <hugo.mig.28@gmail.com>
2016-10-15 15:58:54 -0300

    JustLearningLua

--- astro.lua
+++ astro.lua
@@ -1,3 +1,11 @@
-print("enter a caracter:")
-a = io.read("*caracter")
-print(a)
+function fact (n)
+      if n == 0 then
+        return 1
+      else
+        return n * fact(n-1)
+      end
+    end
+    
+    print("enter a number:")
+    a = io.read("*number")   
+    print(fact(a))
